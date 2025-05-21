8aee3983f52f83dadf7b762768d8ee469546ce50
thaddobi <thaddeus.obi@rci.rogers.ca>
2025-05-21 18:13:24 -0400

    Updated local changes for sidebar items

--- packages/app/src/components/Root/Root.tsx
+++ packages/app/src/components/Root/Root.tsx
@@ -5,7 +5,7 @@ import ExtensionIcon from '@material-ui/icons/Extension';
 import LibraryBooks from '@material-ui/icons/LibraryBooks';
 import CreateComponentIcon from '@material-ui/icons/AddCircleOutline';
 import GitHubIcon from '@material-ui/icons/GitHub';
-import JiraIcon from '@material-ui/icons/GitHub';
+import JiraIcon from '@material-ui/icons/Bathtub';
 import LogoFull from './LogoFull';
 import LogoIcon from './LogoIcon';
 import {
@@ -79,7 +79,7 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
         <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
         <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
         <SidebarItem icon={GitHubIcon} to="catalog" text="Github" />
-        <SidebarItem icon={JiraIcon} to="catalog" text="Jira" />
+        <SidebarItem icon={JiraIcon} to="jira" text="Jira" />
         {/* End global nav */}
         <SidebarDivider />
         <SidebarScrollWrapper>
